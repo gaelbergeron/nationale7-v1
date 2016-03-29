b2b0d29714925eb5223137742cefd74ad4b22317
gaelbergeron <gael.bergeron@gmail.com>
2016-03-29 10:54:52 +0200

    Add confirmation message to inspector contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,10 +7,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to request.referrer, :notice => 'Merci de nous avoir contacté, nous vous recontactons au plus vite !'
     else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
+      redirect_to request.referrer, :notice => 'Oups, on dirait que le site a eu un petit bug ! Nous nous excusons pour la gène occasionnée, vous pouvez nous joindre en tout temps au 07 61 25 80 89' 
     end
   end
 end
