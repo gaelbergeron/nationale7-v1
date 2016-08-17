8a4f91caff246b796c36edec04fddd8d6f9296c7
gaelbergeron <gael.bergeron@gmail.com>
2016-08-17 17:43:26 +0200

    Change rails app name to nationale7-v1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_vroom-mvp_session'
+Rails.application.config.session_store :cookie_store, key: '_nationale7_v1_session'
