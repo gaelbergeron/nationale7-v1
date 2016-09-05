9b05a47a91987f840040f22ec08851e9b8cf6b63
gaelbergeron <gael.bergeron@gmail.com>
2016-09-05 15:38:20 +0200

    Try fix gmail smtp issue 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: 'gmail.com',
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: 'gael.bergeron@gmail.com',
-  password: 'Gmail1988'
+    address: 'smtp-relay.gmail.com',
+    port: 587,
+    domain: 'nationale7.co',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: 'gael@nationale7.co',
+    password: 'Gilerasm50'
   }
 
 end
