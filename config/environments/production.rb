b2f6fab1306b0b7f4a16913b92d9db677d45dccf
gaelbergeron <gael.bergeron@gmail.com>
2016-09-05 16:33:06 +0200

    Try fix gmail smtp issue 15

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 25,
-    domain: 'gmail.com',
+    port: 587,
+    domain: 'nationale7-v1.herokuapp.com',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: 'gael.bergeron@gmail.com',
