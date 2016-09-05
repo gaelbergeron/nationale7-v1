8aa0d946b94377c429f56f84024d8328f8a705cf
gaelbergeron <gael.bergeron@gmail.com>
2016-09-05 14:35:23 +0200

    Setup .env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,16 +43,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default :charset => 'utf-8'
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: 'smtp.gmail.com',
   port: 587,
   domain: 'gmail.com',
-  authentication: "plain",
+  authentication: 'plain',
   enable_starttls_auto: true,
-  user_name: 'gael.bergeron@gmail.com',
-  password: 'Gmail1988'
+  user_name: ENV['GMAIL_USERNAME'],
+  password: ENV['GMAIL_PASSWORD']
   }
 
 end
