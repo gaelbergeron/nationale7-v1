1b322966501ca0ffa47224d1c79c014f83a60a24
gaelbergeron <gael.bergeron@gmail.com>
2016-09-14 20:33:22 +0200

    Fix overflow bug - test 4

--- app/assets/javascripts/media-queries.js
+++ app/assets/javascripts/media-queries.js
@@ -1,13 +1,3 @@
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
-  }
-);
 $(document).ready(function(){
 
 
