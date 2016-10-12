8a75f717077eed649566b2398e1ed973ae423263
gaelbergeron <gael.bergeron@gmail.com>
2016-10-12 15:01:28 +0200

    Test sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -36,11 +36,11 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
   fog_provider: 'AWS',
   aws_access_key_id: 'AKIAI3YND4WPLSICL56Q',
   aws_secret_access_key: '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',
-  fog_directory: 'nationale7v2',
+  fog_directory: 'nationale7v1',
   fog_region: 'eu-west-1'
 )
 
-SitemapGenerator::Sitemap.sitemaps_host = "https://s3-eu-west-1.amazonaws.com/nationale7v2/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://nationale7v1.s3.amazonaws.com/"
 
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
