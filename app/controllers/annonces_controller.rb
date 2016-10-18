2b68d082437e6f617c7e2557ddaa243db061bc22
gaelbergeron <gael.bergeron@gmail.com>
2016-10-18 17:42:54 +0200

    Almost finish styling annonce contenu page

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -6,6 +6,8 @@ class AnnoncesController < ApplicationController
 
 	def show
 		@voiture_annonce = Car.friendly.find(params[:id])
+    @caroptions = CarOption.where(car_id: @voiture_annonce.id)
+    @options = Option.where(id: @caroptions.map(&:option_id))
 	end
 
 end
