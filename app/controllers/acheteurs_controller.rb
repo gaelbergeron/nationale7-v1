f090874540b4e538e7677b1e44afe4cb039afabb
gaelbergeron <gael.bergeron@gmail.com>
2016-11-07 20:14:31 +0100

    Update the annonces template

--- app/controllers/acheteurs_controller.rb
+++ app/controllers/acheteurs_controller.rb
@@ -11,7 +11,7 @@ class AcheteursController < ApplicationController
     @acheteur = Acheteur.new(params[:acheteur])
     @acheteur.request = request
     if @acheteur.deliver
-      redirect_to request.referrer, :notice => 'Merci de nous avoir contacté, nous vous recontacterons dans les plus brefs délais !', :class => :success
+      redirect_to request.referrer, :notice => 'Merci d\'avoir souscrit à nos alertes acheteurs ! Nous espérons vous trouver la perle rare au plus vite !', :class => :success
     else
       redirect_to request.referrer, :alert => 'Oups, on dirait que le site a eu un petit bug ! Nous nous excusons pour la gène occasionnée, vous pouvez nous joindre en tout temps au 07 61 25 80 89' 
     end
--- app/models/acheteur.rb
+++ app/models/acheteur.rb
@@ -10,17 +10,34 @@ class Acheteur < MailForm::Base
   attribute :kilometrage_max_1
   attribute :prix_min_1
   attribute :prix_max_1
+  
+  attribute :type_2
+  attribute :marque_2
+  attribute :modele_2
+  attribute :transmission_2
+  attribute :carburant_2
+  attribute :annee_min_2
+  attribute :annee_max_2
+  attribute :kilometrage_min_2
+  attribute :kilometrage_max_2
+  attribute :prix_min_2
+  attribute :prix_max_2
+
+  attribute :type_3
+  attribute :marque_3
+  attribute :modele_3
+  attribute :transmission_3
+  attribute :carburant_3
+  attribute :annee_min_3
+  attribute :annee_max_3
+  attribute :kilometrage_min_3
+  attribute :kilometrage_max_3
+  attribute :prix_min_3
+  attribute :prix_max_3
 
   attribute :nom
   attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :phone_number
-  attribute :description
-
-  # attribute :modele
-  # attribute :annee
-  # attribute :motorisation
-  # attribute :finition
-  # attribute :kilometrage
   attribute :code_postal
   attribute :nickname,  :captcha  => true
   attribute :details
